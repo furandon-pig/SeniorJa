166abd81be262d723db034c136ddb847a6590904
hiro2021 <yoshino.hirokazu444@gmail.com>
2023-04-25 14:07:35 +0900

    lesson3見直し

---
+++ lesson/js/lesson3/3code00.js
@@ -0,0 +1,14 @@
+/*written by hiro2021*/
+// LessonコメントをHTMLのｐタグに追記しています
+var coment = document.getElementById('coment');
+var com1 = ["<br>3code01で、画面スライドを本体とナビで表示する練習をします。"];
+var com2 = ["<br>3code02で、画面スライドは何処が違うのか、VScodeのファイル比較を練習します。"];
+var com3 = ["<br>3code03で、BootStrapで画面スライドを練習する予定です。"];
+var com4 = ["<br>3code04で、Flexboxで画面スライドを練習する予定です。"];
+var com5 = ["<br>"];
+var com6 = ["<br>"];
+var com7 = ["<br>"];
+var com8 = ["<br>"];
+var com9 = ["<br>2code09は練習問題です。力試しです。もくもく会の時に発表して下さい！"];
+var com0 = ["lesson3.htmlのscript内の3code**.jsの番号を変更して動かします。<br>"];
+coment.innerHTML = [com0 + com1 + com2 + com3 + com4 + com5 + com6 + com7 + com8 + com9];
--- lesson/js/lesson3/3code01.js
+++ lesson/js/lesson3/3code01.js
@@ -1,7 +1,5 @@
 /*written by hiro2021*/
 // スライダーに表示する画像のパス
-// lesson3.html の<link rel="stylesheet" href="reset.css">がなくてもよいようにするには、どこを直せばよいか？
-//
 var imgList = [
 	"images/img01.jpg",
 	"images/img02.jpg",
@@ -9,7 +7,7 @@ var imgList = [
 	"images/img04.jpg"
 ];
 
-// 配列の数だけ繰り返し
+// 配列の数だけ繰り返し画像とナビの要素を自動で追加
 for(var i = 0; i < imgList.length; i++) {
 	// li要素を取得
 	var slide = document.createElement("li");
@@ -22,13 +20,7 @@ for(var i = 0; i < imgList.length; i++) {
 	var nav = document.createElement("li");
 	// プロパティ「data-nav-index」に数値を割り振る
 	nav.setAttribute("data-nav-index", i);
-
-	// 01からの変更箇所 ここから
-	nav.style.backgroundImage = "url(" + imgList[i] + ")";
-	nav.style.width = 100 / imgList.length + "%";
-	// 01からの変更箇所 ここまで
 	
-    console.log(imgList.length);
 	// li要素をクラス名「nav」の子要素として追加
 	document.getElementsByClassName("nav")[0].appendChild(nav);
 }
@@ -36,42 +28,34 @@ for(var i = 0; i < imgList.length; i++) {
 // スライドの数を取得(処理のために-1する)
 var length = imgList.length - 1;
 
-// クラス名「slider」にスライドの1枚毎の要素を格納
+// クラス名「imageSlide」に画像の1枚毎の要素を格納
 var imageSlide = document.getElementsByClassName("slider-inner")[0].getElementsByTagName("li");
 
-// クラス名「nav」にドットナビの1つずつの要素を格納
+// クラス名「dotNavigation」にドットナビの1つの要素を格納
 var dotNavigation = document.getElementsByClassName("nav")[0].getElementsByTagName("li");
 
 // 「現在○○枚目のスライドを表示している」というインデックス番号を格納する変数
 var nowIndex = 0;
-
-// スライドがアニメーション中か判断するフラグ
-var isChanging = false;
-
-// スライドのsetTimeoutを管理するタイマー
-var slideTimer;
-
 // 現在表示されている画像とドットナビにクラス名を付ける
 imageSlide[nowIndex].classList.add("show");
 dotNavigation[nowIndex].classList.add("current");
-
+// スライドがアニメーション中か判断するフラグ
+var isChanging = false;
+// スライドのsetTimeoutを管理するタイマー
+var slideTimer;
 // スライド切り替え時に呼び出す関数
 function sliderSlide(val) {
-	if (isChanging) {
+	if (isChanging === true) {
 		return false;
 	}
 	isChanging = true;
-
 	// 現在表示している画像とナビからクラス名を削除
 	imageSlide[nowIndex].classList.remove("show");
 	dotNavigation[nowIndex].classList.remove("current");
-
 	nowIndex = val;
-
 	// 次に表示するスライドとナビにカレントクラスを設定
 	imageSlide[nowIndex].classList.add("show");
 	dotNavigation[nowIndex].classList.add("current");
-
 	// アニメーションが終わるタイミングでisChangingのステータスをfalseに
 	slideTimer = setTimeout(function(){
 		isChanging = false;
@@ -81,16 +65,19 @@ function sliderSlide(val) {
 // 左矢印のナビをクリックした時のイベント
 document.getElementById("arrow-prev").addEventListener("click", function(){
 	var index = nowIndex - 1;
-	if(index < 0) index = length;
+	if(index < 0){
+	  index = length;
+	}
 	sliderSlide(index);
 }, false);
 // 右矢印のナビをクリックした時のイベント
 document.getElementById("arrow-next").addEventListener("click", function(){
 	var index = nowIndex + 1;
-	if(index > length) index = 0;
+	if(index > length){
+	  index = 0;
+	}
 	sliderSlide(index);
 }, false);
-
 // ドットナビをクリックした時のイベントを作成
 for(var i = 0; i < dotNavigation.length; i++) {
 	// データ属性のインデックス番号を元にスライドを行う
