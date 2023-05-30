213642ce7d7f08d26ef970cc22bb8b3c0b37ade0
hiro2021 <yoshino.hirokazu444@gmail.com>
2023-05-30 12:54:09 +0900

    yachiyokomatsuさんの提供

---
+++ step up/dress_up/mackk.js
@@ -0,0 +1,326 @@
+
+function tasizan() {
+	text01 = document.getElementById('atai1');
+	text02 = document.getElementById('atai2');
+
+	x = parseInt(text01.value);
+	y = parseInt(text02.value);
+
+	z = x + y;
+	
+	kekka = document.getElementById('kekka');
+	kekka.innerHTML = z;
+	kekka.style.color ='green';
+
+	let a = z== 20;
+	let b = z==40;
+	let c = z==60;
+	let d = z==80;
+	let e = z==100;
+	let h = z==120;
+
+if (a) {
+
+	if(b,c,d,e.h)
+	{
+		false;
+	}
+	kekka.style.color = 'red';
+
+	const canvas = document.createElement('canvas')
+	const ctx =canvas.getContext('2d');
+
+	canvas.width = 250;
+	canvas.height = 420;
+
+
+	const img = new Image();
+	img.src = "rika.png";
+
+	//画像をcanvasに設定
+	img.onload = function(){
+	ctx.drawImage(img, 0, 0, 250, 420);
+
+
+	canvas.setAttribute('style' , 'display:block;margin:auto');
+	document.body.appendChild(canvas);
+	}
+}
+if (b) {
+
+	if(c,d,e.h)
+	{
+		false;
+	}
+	kekka.style.color = 'red';
+
+	const canvas = document.createElement('canvas')
+	const ctx =canvas.getContext('2d');
+
+	canvas.width = 250;
+	canvas.height = 420;
+
+
+	const img = new Image();
+	img.src = "rika12.png";
+
+	//画像をcanvasに設定
+	img.onload = function(){
+	ctx.drawImage(img, 0, 0, 250, 420);
+
+
+	canvas.setAttribute('style' , 'display:block;margin:auto');
+	document.body.appendChild(canvas);
+	}
+}
+
+if (c) {
+	kekka.style.color = 'red';
+	const canvas = document.createElement('canvas')
+	const ctx =canvas.getContext('2d');
+
+	canvas.width = 250;
+	canvas.height = 420;
+
+	const img = new Image();
+	img.src = "rika13.png";
+
+	//画像をcanvasに設定
+	img.onload = function(){
+	ctx.drawImage(img, 0, 0, 250, 420);
+
+
+	canvas.setAttribute('style' , 'display:block;margin:auto');
+	document.body.appendChild(canvas);
+	}
+}
+
+
+ if (d) {
+	kekka.style.color = 'red';
+	const canvas = document.createElement('canvas')
+	const ctx =canvas.getContext('2d');
+
+	canvas.width = 250;
+	canvas.height = 420;
+
+	const img = new Image();
+	img.src = "rika15.png";
+
+	//画像をcanvasに設定
+	img.onload = function(){
+	ctx.drawImage(img, 0, 0, 250, 420);
+
+	canvas.setAttribute('style' , 'display:block;margin:auto');
+	document.body.appendChild(canvas);
+	}
+}
+
+if (e) {
+	kekka.style.color = 'red';
+	const canvas = document.createElement('canvas')
+	const ctx =canvas.getContext('2d');
+
+	canvas.width = 250;
+	canvas.height = 420;
+
+
+	const img = new Image();
+	img.src = "rika16.png";
+
+	//画像をcanvasに設定
+	img.onload = function(){
+	ctx.drawImage(img, 0, 0, 250, 420);
+
+
+	canvas.setAttribute('style' , 'display:block;margin:auto');
+	document.body.appendChild(canvas);
+
+	}
+}
+if (h) {
+	kekka.style.color = 'red';
+	const canvas = document.createElement('canvas')
+	const ctx =canvas.getContext('2d');
+
+	canvas.width = 250;
+	canvas.height = 420;
+
+
+	const img = new Image();
+	img.src = "rika17.png";
+
+	//画像をcanvasに設定
+	img.onload = function(){
+	ctx.drawImage(img, 0, 0, 250, 420);
+
+
+	canvas.setAttribute('style' , 'display:block;margin:auto');
+	document.body.appendChild(canvas);
+	}
+}
+}
+
+function hikizan() {
+	text03 = document.getElementById('atai3');
+	text04 = document.getElementById('atai4');
+
+	a = parseInt(text03.value);
+	b = parseInt(text04.value);
+
+	z = a - b;
+
+	kekka2 = document.getElementById('kekka2');
+	kekka2.innerHTML = z;
+	kekka2.style.color ='green';
+
+	a = z== 40;
+	b = z==60;
+	c = z==80;
+	d = z==100;
+
+if (d) {
+	kekka2.style.color ='red';
+	const canvas = document.createElement('canvas')
+	const ctx =canvas.getContext('2d');
+
+	canvas.width = 250;
+	canvas.height = 420;
+
+
+	const img = new Image();
+	img.src = "rika16.png";
+
+	//画像をcanvasに設定
+	img.onload = function(){
+	ctx.drawImage(img, 0, 0, 250, 420);
+
+
+	canvas.setAttribute('style' , 'display:block;margin:auto');
+	document.body.appendChild(canvas);
+	}
+}
+
+if (c) {
+	kekka2.style.color ='red';
+	const canvas = document.createElement('canvas')
+	const ctx =canvas.getContext('2d');
+
+	canvas.width = 250;
+	canvas.height = 420;
+
+
+	const img = new Image();
+	img.src = "rika15.png";
+
+	//画像をcanvasに設定
+	img.onload = function(){
+	ctx.drawImage(img, 0, 0, 250, 420);
+
+
+	canvas.setAttribute('style' , 'display:block;margin:auto');
+	document.body.appendChild(canvas);
+	}
+}
+
+if (b) {
+	kekka2.style.color = 'red';
+	const canvas = document.createElement('canvas')
+	const ctx =canvas.getContext('2d');
+
+	canvas.width = 250;
+	canvas.height = 420;
+
+	const img = new Image();
+	img.src = "rika13.png";
+
+	//画像をcanvasに設定
+	img.onload = function(){
+	ctx.drawImage(img, 0, 0, 250, 420);
+
+	canvas.setAttribute('style' , 'display:block;margin:auto');
+	document.body.appendChild(canvas);
+	}
+}
+
+if (a) {
+	kekka2.style.color ='red';
+	const canvas = document.createElement('canvas')
+	const ctx =canvas.getContext('2d');
+
+	canvas.width = 250;
+	canvas.height = 420;
+
+
+	const img = new Image();
+	img.src = "rika12.png";
+
+	//画像をcanvasに設定
+	img.onload = function(){
+	ctx.drawImage(img, 0, 0, 250, 420);
+
+
+	canvas.setAttribute('style' , 'display:block;margin:auto');
+	document.body.appendChild(canvas);
+	}
+}
+
+if (z == 20) {
+	kekka2.style.color = 'red';
+	const canvas = document.createElement('canvas')
+	const ctx =canvas.getContext('2d');
+
+	canvas.width = 250;
+	canvas.height = 420;
+
+
+	const img = new Image();
+	img.src = "rika.png";
+
+	//画像をcanvasに設定
+	img.onload = function(){
+	ctx.drawImage(img, 0, 0, 250, 420);
+
+
+	canvas.setAttribute('style' , 'display:block;margin:auto');
+	document.body.appendChild(canvas);
+
+	}
+ }
+}
+
+function clearText() {
+	var textForm = document.getElementById("atai1");
+	textForm.value = '';
+	var textForm = document.getElementById("atai2");
+	textForm.value = '';
+	var textForm = document.getElementById("atai3");
+	textForm.value = '';
+	var textForm = document.getElementById("atai4");
+	textForm.value = '';
+}
+
+function reset() {
+ 	location.href = "toka.html";
+}
+
+function kaishi() {
+	kekka.style.color = 'red';
+
+	const canvas = document.createElement('canvas')
+	const ctx =canvas.getContext('2d');
+
+	canvas.width = 250;
+	canvas.height = 420;
+
+	const img = new Image();
+	img.src = "rika.png";
+
+	//画像をcanvasに設定
+	img.onload = function(){
+	ctx.drawImage(img, 0, 0, 250, 420);
+
+	canvas.setAttribute('style' , 'display:block;margin:auto');
+	document.body.appendChild(canvas);
+	}
+}
